cc51626ec433bceb55c73118183f32ac4e921b8f
mpediredla <mpediredla@miraclesoft.com>
2025-07-15 20:06:41 +0530

    Adusted the unwanted paddings and margins to entire application

--- src/App.js
+++ src/App.js
@@ -1,9 +1,15 @@
 import React, { useState } from 'react';
 import Dashboard from './pages/Dashboard';
+import { Box, CssBaseline } from '@mui/material';
 
 function App() {
-  return ( 
-    <Dashboard/>
+  return (
+    <>
+      <CssBaseline />
+      <Box sx={{ p: 0, m: 0, height: '100vh' }}>
+        <Dashboard />
+      </Box>
+    </>
   );
 }
 
